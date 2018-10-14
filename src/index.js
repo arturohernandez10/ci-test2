0a5c93fec80661f6ad0183b70b8879bb49976bdf
artur <arturohernandez@clear-launch.com>
2018-10-13 22:10:11 -0500

    initial

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import rootReducer from './reducers'
+import { devToolsEnhancer  } from 'redux-devtools-extension'
+import data from "./database.js"
+
+
+fetch("./database").then( (response) => {
+    response.json().then( (json) => {
+
+      const store = createStore(rootReducer, json, devToolsEnhancer());
+
+      render(
+        <Provider store={store}>
+          <App />
+        </Provider>,
+        document.getElementById('root')
+      )
+    })
+  })
